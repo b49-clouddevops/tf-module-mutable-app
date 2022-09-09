07f165542fddb030d63b9a601db4039461480433
mvpc-11232 <mvp2612@hotmail.com>
2022-09-09 08:08:44 +0530

    Auto Push Commit

--- sg.tf
+++ sg.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "allow_app" {
     from_port   = var.APP_PORT
     to_port     = var.APP_PORT
     protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, var.WORKSPATION_IP]
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }
 
   ingress {
--- vars.tf
+++ vars.tf
@@ -3,6 +3,9 @@ variable "ENV" {}
 variable "SPOT_INSTANCE_COUNT" {}
 variable "OD_INSTANCE_COUNT" {}
 variable "COMPONENT" {}
+variable "APP_PORT" {}
+variable "WORKSPATION_IP" {}
+
 
 
 
