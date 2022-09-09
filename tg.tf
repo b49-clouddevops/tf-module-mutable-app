78b9770054642eaf6f9c135b1d4cbbf7a5256e72
mvpc-11232 <mvp2612@hotmail.com>
2022-09-09 09:23:55 +0530

    Auto Push Commit

--- tg.tf
+++ tg.tf
@@ -1,6 +1,9 @@
+# Creates TG 
 resource "aws_lb_target_group" "app" {
   name     = "${var.COMPONENT}-${var.ENV}"
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  vpc_id   =data.terraform_remote_state.vpc.outputs.VPC_ID 
 }
+
+# Now attachi instances to the created target.
