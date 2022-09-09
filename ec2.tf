9172c15cbb3c69f4ab03c47f6ce9e6b878bd8c62
mvpc-11232 <mvp2612@hotmail.com>
2022-09-09 08:11:00 +0530

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,8 @@ resource "aws_spot_instance_request" "spot" {
   ami                     = data.aws_ami.ami
   instance_type           = var.INSTANCE_TYPE
   wait_for_fulfillment    = true 
-
+  vpc_security_group_ids  = [aws_security_group.allow_app.id]
+  subnet_id               
   tags = {
     Name = "${var.COMPONENT}-${ENV}"
   }
