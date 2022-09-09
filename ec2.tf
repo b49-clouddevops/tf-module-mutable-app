c3d444429a243c41ccb5aed38ecee746b56a8786
mvpc-11232 <mvp2612@hotmail.com>
2022-09-09 08:31:22 +0530

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 # Request a spot instance
 resource "aws_spot_instance_request" "spot" {
   count                   = var.SPOT_INSTANCE_COUNT  # whenever you use count, you need to use element to pick form the list.
-  ami                     = data.aws_ami.ami
+  ami                     = data.aws_ami.ami.id
   instance_type           = var.INSTANCE_TYPE
   wait_for_fulfillment    = true 
   vpc_security_group_ids  = [aws_security_group.allow_app.id]
