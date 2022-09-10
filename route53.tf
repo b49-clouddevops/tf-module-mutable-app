bf9ba4f95071b10b3208275b2144a3341847648a
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 08:19:19 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,9 +1,9 @@
 # This needs the DNS NAme of your ALB 
 
 resource "aws_route53_record" "record" {
-  zone_id = var.LB_TYPE == internal ? data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID :  data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PUBLIC_ID 
+  zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID
   name    = "${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"
-  type    = "A"
+  type    = "CNAME"
   ttl     = 60
   records = var.LB_TYPE == internal ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
 }
