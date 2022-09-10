ba1718d17a9466db862369cb6cc7d98a4812fbc8
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 09:05:05 +0530

    Auto Push Commit

--- tg.tf
+++ tg.tf
@@ -29,3 +29,13 @@ resource "aws_lb_listener_rule" "app_rule" {
     }
   }
 }
+
+
+# The following example shows how to generate a random priority
+# between 1 and 50000 for a aws_alb_listener_rule resource:
+
+resource "random_integer" "priority" {
+  min = 100
+  max = 500
+  }
+}
