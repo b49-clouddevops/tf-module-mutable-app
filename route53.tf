150f19b612d6a7bb8e2c489213e20dffa17319cd
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 08:22:39 +0530

    first push

--- route53.tf
+++ route53.tf
@@ -5,7 +5,7 @@ resource "aws_route53_record" "record" {
   name    = "${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"
   type    = "CNAME"
   ttl     = 60
-  records = var.LB_TYPE == internal ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
+  records = var.LB_TYPE == "internal" ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
 }
 
 
