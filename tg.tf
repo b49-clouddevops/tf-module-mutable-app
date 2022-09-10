83c93fcc9701c8748df3730a649bdfbdbf3b26e6
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 07:53:55 +0530

    Auto Push Commit

--- tg.tf
+++ tg.tf
@@ -13,3 +13,25 @@ resource "aws_lb_target_group_attachment" "instance-attach" {
   target_id        = element(local.ALL_INSTANCE_IDS, count.index)
   port             = 8080
 }
+
+resource "aws_lb_listener_rule" "static" {
+  listener_arn = aws_lb_listener.front_end.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.static.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/static/*"]
+    }
+  }
+
+  condition {
+    host_header {
+      values = ["example.com"]
+    }
+  }
+}
