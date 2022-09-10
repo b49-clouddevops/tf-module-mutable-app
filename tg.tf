1038b085dc01da0d75b38c7a34ef3e40b283bcd4
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 13:40:37 +0530

    first push

--- tg.tf
+++ tg.tf
@@ -3,7 +3,7 @@ resource "aws_lb_target_group" "app" {
   name     = "${var.COMPONENT}-${var.ENV}"
   port     = 8080
   protocol = "HTTP"
-  vpc_id   =data.terraform_remote_state.vpc.outputs.VPC_ID 
+  vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID 
 }
 
 # Now attachi instances to the created target.
