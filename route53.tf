182a295230a037810be1b8e0900283d29acc4192
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 08:06:37 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,1 +1,9 @@
-# This needs the CRM or the DNS NAme of your ALB 
+# This needs the DNS NAme of your ALB 
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z09626353E72G6GNQ0R5A"
+  name    = "${var.COMPONENT}-dev.roboshop.internal"
+  type    = "A"
+  ttl     = 60
+  records = [aws_spot_instance_request.cheap_worker.private_ip]
+}
