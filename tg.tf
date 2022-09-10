e5594b1b4018f137f6ba2a3fd83b5264b82db9bf
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 09:04:07 +0530

    Auto Push Commit

--- tg.tf
+++ tg.tf
@@ -16,7 +16,7 @@ resource "aws_lb_target_group_attachment" "instance-attach" {
 
 resource "aws_lb_listener_rule" "app_rule" {
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
-  priority     = 100
+  priority     = 100  
 
   action {
     type             = "forward"
