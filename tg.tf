b1b2d87d492685d4fc078166a3a07e33ee98f6e0
mvpc-11232 <mvp2612@hotmail.com>
2022-09-11 07:50:37 +0530

    Auto Push Commit

--- tg.tf
+++ tg.tf
@@ -17,7 +17,7 @@ resource "aws_lb_target_group_attachment" "instance-attach" {
 # Private Listener rules
 # Public Listener , creates only if the LB_TYPE is internal
 resource "aws_lb_listener_rule" "app_rule" {
-  count        = var.LB_TYPE == "intrenal" ? 1 : 0
+  count        = var.LB_TYPE == "internal" ? 1 : 0
 
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
   priority     = random_integer.lb-rule-priority.result
